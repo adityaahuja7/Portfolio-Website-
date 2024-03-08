e9e41236cf812ccf6ef789c172d5221777f13df0
adityaahuja7 <aditya20275@iiitd.ac.in>
2024-03-08 18:41:34 +0530

    Made image smaller.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -132,8 +132,8 @@ export default function Home() {
           <Image
             className=" hidden md:mt-5 md:block"
             src="/home_image.png"
-            width={680}
-            height={680}
+            width={620}
+            height={620}
             alt="Picture of the author"
             priority
           />
